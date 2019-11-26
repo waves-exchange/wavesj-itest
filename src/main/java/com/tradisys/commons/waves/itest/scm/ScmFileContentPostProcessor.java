fb0daec261e4861e0699b164067d45cfceadd409
amurashko <a.murashko@tradisys.com>
2019-11-26 11:41:21 +0300

    replace line utility method

--- src/main/java/com/tradisys/commons/waves/itest/scm/ScmFileContentPostProcessor.java
+++ src/main/java/com/tradisys/commons/waves/itest/scm/ScmFileContentPostProcessor.java
@@ -2,4 +2,13 @@ package com.tradisys.commons.waves.itest.scm;
 
 public interface ScmFileContentPostProcessor {
     StringBuilder modify(StringBuilder fileContent);
+
+    static void replaceLine(String oldLinePrefix, String newLine, StringBuilder in) {
+        int start = in.indexOf(oldLinePrefix);
+        int end = in.indexOf("\n", start);
+
+        if (start > 0) {
+            in.replace(start, end, newLine);
+        }
+    }
 }
